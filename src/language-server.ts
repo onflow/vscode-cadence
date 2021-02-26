2800ad78f1103fe2efe2500dc9f478ccd9825093
Max Daunarovich <max.daunarovich@gmail.com>
2021-02-26 21:13:46 +0300

    Update LSP

--- src/language-server.ts
+++ src/language-server.ts
@@ -1,18 +1,28 @@
 import {LanguageClient} from "vscode-languageclient";
-import {ExtensionContext, window} from "vscode";
+import {ExtensionContext, window, commands} from "vscode";
 import {Config} from "./config";
-import {CREATE_ACCOUNT_SERVER, CREATE_DEFAULT_ACCOUNTS_SERVER, SWITCH_ACCOUNT_SERVER} from "./commands";
+import { 
+    CREATE_ACCOUNT_SERVER, 
+    CREATE_DEFAULT_ACCOUNTS_SERVER, 
+    SWITCH_ACCOUNT_SERVER, 
+    DEPLOY_CONTRACT,
+    EXECUTE_SCRIPT,
+    SEND_TRANSACTION,
+    CHANGE_EMULATOR_STATE
+} from "./commands";
+import { EmulatorState } from './extension'
 
 // The args to pass to the Flow CLI to start the language server.
 const START_LANGUAGE_SERVER_ARGS = ["cadence", "language-server"];
 
+
 export class LanguageServerAPI {
     client: LanguageClient;
 
-    constructor(ctx: ExtensionContext, config: Config) {
+    constructor(ctx: ExtensionContext, config: Config, emulatorState: EmulatorState) {
         this.client = new LanguageClient(
             "cadence",
-            "Cadence",
+            "Cadence", 
             {
                 command: config.flowCommand,
                 args: START_LANGUAGE_SERVER_ARGS,
@@ -22,13 +32,21 @@ export class LanguageServerAPI {
                 synchronize: {
                     configurationSection: "cadence"
                 },
-                initializationOptions: config.serverConfig,
+                initializationOptions: {
+                    ...config.serverConfig,
+                    emulatorState
+                }
             }
         );
 
         this.client
             .onReady()
             .then(() => {
+                
+                this.client.onNotification(DEPLOY_CONTRACT, this.deployContract);
+                this.client.onNotification(EXECUTE_SCRIPT, this.executeScript);
+                this.client.onNotification(SEND_TRANSACTION, this.sendTransaction);
+
                 return window.showInformationMessage("Cadence language server started");
             })
             .catch(err => {
@@ -39,6 +57,44 @@ export class LanguageServerAPI {
 
         const clientDisposable = this.client.start();
         ctx.subscriptions.push(clientDisposable);
+    }
+
+
+    // Executes command to deploy contract with provided arguments
+    async deployContract(...params: any){
+        console.log({params})
+        return commands.executeCommand(
+            DEPLOY_CONTRACT,
+            params
+        )
+    }
+
+    // Sends a request to execute script with provided arguments
+    async executeScript(args: string){
+        console.log({args})
+        return commands.executeCommand(
+            EXECUTE_SCRIPT,
+            args
+        )
+    }
+
+    // Executes command to send transaction with provided arguments
+    async sendTransaction(args: any, signers: any){        
+        return commands.executeCommand(
+            SEND_TRANSACTION,
+            args,
+            signers
+        )
+    }
+
+    async changeEmulatorState(emulatorState: EmulatorState) {
+        console.log({ emulatorState })
+        return this.client.sendRequest("workspace/executeCommand", {
+            command: CHANGE_EMULATOR_STATE,
+            arguments:[
+                emulatorState
+            ]
+        })
     }
 
     // Sends a request to switch the currently active account.
