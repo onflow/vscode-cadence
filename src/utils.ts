fcc5fe25cad1ad0b024e056ed50138290e4cfb1a
Max Daunarovich <max.daunarovich@gmail.com>
2021-03-03 18:58:03 +0300

    Refactor makeArgsFlag to accept list of strings

--- src/utils.ts
+++ src/utils.ts
@@ -1,12 +1,21 @@
-export const makeArgsFlag = (args: string) => {
-    if (args !== ""){
-        const escapedArgs = args.replace(/"/g,'\\"')
-        return `--args="[${escapedArgs}]"`
+export const makeArgsFlag = (args: string[]) => {
+    if (args.length > 0) {
+      const escapedArgs = args
+        .map((arg) => {
+          const clean = stripLineEnd(arg)
+            .replace(/[\w\d]+(?=:)/g,'"$&"')
+            .replace(/"/g, '\\"')
+            .replace(/\\/g,'')
+          const json = JSON.parse(clean)
+          return JSON.stringify(json).replace(/"/g,'\\"')
+        })
+        .join(",");
+      return `--args="[${escapedArgs}]"`;
     }
-
-    return ""
-}
-
+  
+    return "";
+  };
+  
 export const makeFlag = (flagName: string) => (flagValue: string) => {
     return flagValue ? `--${flagName}="${flagValue}"` : ""
 }
