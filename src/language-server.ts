bf5c3e6eae9f9ea3566f7a321b625a74084a36b1
Max Daunarovich <max.daunarovich@gmail.com>
2021-03-26 06:05:37 +0300

    Add initAccountManager to language client

--- src/language-server.ts
+++ src/language-server.ts
@@ -1,14 +1,12 @@
-import { LanguageClient } from "vscode-languageclient";
+import { LanguageClient, StateChangeEvent } from "vscode-languageclient";
 import { ExtensionContext, window, commands } from "vscode";
 import { Config } from "./config";
 import {
     CREATE_ACCOUNT_SERVER,
     CREATE_DEFAULT_ACCOUNTS_SERVER,
     SWITCH_ACCOUNT_SERVER,
-    DEPLOY_CONTRACT,
-    EXECUTE_SCRIPT,
-    SEND_TRANSACTION,
-    CHANGE_EMULATOR_STATE
+    CHANGE_EMULATOR_STATE,
+    INIT_ACCOUNT_MANAGER
 } from "./commands";
 import { EmulatorState } from './extension'
 
@@ -18,8 +16,12 @@ const START_LANGUAGE_SERVER_ARGS = ["cadence", "language-server"];
 
 export class LanguageServerAPI {
     client: LanguageClient;
+    running: boolean
 
     constructor(ctx: ExtensionContext, config: Config, emulatorState: EmulatorState) {
+        // Init running state with false and update, when client is connected to server
+        this.running = false
+
         this.client = new LanguageClient(
             "cadence",
             "Cadence",
@@ -50,8 +52,29 @@ export class LanguageServerAPI {
                 );
             });
 
+        this.client.onDidChangeState((e: StateChangeEvent) => {
+            switch(e.newState){
+                case 1:
+                    this.running = false
+                    break;
+                case 2:
+                    this.running = true
+                    break;
+                default:
+                    this.running = false
+                    break
+            }
+        })
+
         const clientDisposable = this.client.start();
         ctx.subscriptions.push(clientDisposable);
+    }
+
+    async initAccountManager() {
+        return this.client.sendRequest("workspace/executeCommand", {
+            command: INIT_ACCOUNT_MANAGER,
+            arguments: []
+        })
     }
 
     async changeEmulatorState(emulatorState: EmulatorState) {
