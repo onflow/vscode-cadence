e0776c7877b5a6941daede39765fee59f674ffa1
Max Daunarovich <max.daunarovich@gmail.com>
2021-04-05 20:11:03 +0300

    Move accounts to separate file

---
+++ src/account.ts
@@ -0,0 +1,36 @@
+import { addAddressPrefix } from "./address";
+
+// An account that can be used to submit transactions.
+export class Account {
+    index: number;
+    address: string;
+    name: string;
+  
+    constructor(address: string, name: string, index: number | null) {
+      this.address = address;
+      this.name = name;
+
+      if (index != null){
+        this.index = index
+      } else {
+        this.index = -1
+      }
+    }
+
+    setIndex(index: number){
+      this.index = index
+    }
+  
+    getAddress(withPrefix: boolean = true): string {
+      return withPrefix ? `0x${this.address}` : this.address
+    }
+  
+    getName(): string {
+      const name = this.name || `Account ${this.index + 1}`
+      return `${name[0].toUpperCase()}${name.slice(1)}`
+    }
+  
+    fullName(): string {
+      return `${this.getName()} (${addAddressPrefix(this.address)})`;
+    }
+  }
