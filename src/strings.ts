651d86ec65aeccf421cb1e12dc5284c652a5b515
Max Daunarovich <max.daunarovich@gmail.com>
2021-04-07 15:21:24 +0300

    Move prefixes to strings file

--- src/strings.ts
+++ src/strings.ts
@@ -1,1 +1,7 @@
+// Preffix to indicate which account is active
+export const ACTIVE_PREFIX = "🟢";
+export const INACTIVE_PREFIX = "⚫️"
+export const ADD_NEW_PREFIX = "🐣"
+
 export const COPY_ADDRESS = "Copy Address"
+export const CREATE_NEW_ACCOUNT = "Create New Account"
