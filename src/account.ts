965988ba5b46c7f3b7f13111b8f5c5465c207605
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2021-08-06 15:39:15 +0200

    add docs

--- src/account.ts
+++ src/account.ts
@@ -1,6 +1,6 @@
-const addressPrefix = '0x'
-
-// An account that can be used to submit transactions.
+/**
+ * Account representing an address on Flow network
+ */
 export class Account {
   index: number
   address: string
@@ -16,6 +16,11 @@ export class Account {
     this.index = index
   }
 
+  /**
+   * Get address of the account
+   * @param {boolean} withPrefix return address with or without prefix
+   * @returns {string} address
+   */
   getAddress (withPrefix: boolean = true): string {
     if (this.address.includes('0x')) {
       return withPrefix ? this.address : this.address.replace('0x', '')
@@ -24,11 +29,19 @@ export class Account {
     }
   }
 
+  /**
+   * Get name of the account
+   * @returns {string} name
+   */
   getName (): string {
     const name = this.name === '' ? `Account ${this.index + 1}` : this.name
     return `${name[0].toUpperCase()}${name.slice(1)}`
   }
 
+  /**
+   * Get account name, consists of name and address
+   * @returns {string} full name
+   */
   fullName (): string {
     return `${this.getName()} (${this.getAddress()})`
   }
