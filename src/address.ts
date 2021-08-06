cddcfc12312b4839a955fc821df168c40d187c5c
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2021-08-06 15:14:50 +0200

    remove unused files

--- src/address.ts
+++
@@ -1,9 +0,0 @@
-const addressPrefix = '0x'
-
-export function addAddressPrefix (address: string): string {
-  if (address.slice(0, 2) === addressPrefix) {
-    return address
-  }
-
-  return addressPrefix + address
-}
