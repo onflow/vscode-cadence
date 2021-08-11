17d2a5d18a5fa97e0dcf3582e7bca8b0a191d02e
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2021-08-11 16:58:09 +0200

    move unit tests

---
+++ src/test/account.unit.test.ts
@@ -0,0 +1,22 @@
+import { Account } from '../account'
+import * as assert from 'assert'
+
+describe('AccountUnitTest', () => {
+  it('creates an account', () => {
+    const a1 = new Account('Foo', '0x1')
+    a1.setIndex(2)
+
+    assert.strictEqual(a1.address, '0x1')
+    assert.strictEqual(a1.name, 'Foo')
+    assert.strictEqual(a1.getName(), a1.name)
+    assert.strictEqual(a1.fullName(), `${a1.name} (${a1.address})`)
+    assert.strictEqual(a1.index, 2)
+
+    assert.strictEqual(a1.getAddress(), '0x1')
+    assert.strictEqual(a1.getAddress(false), '1')
+
+    a1.address = '1'
+    assert.strictEqual(a1.getAddress(), '0x1')
+    assert.strictEqual(a1.getAddress(false), '1')
+  })
+})
