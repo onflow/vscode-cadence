553779ee70a7fd96c7d3582ff8cb2c0b785c128e
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2021-08-11 17:03:14 +0200

    check if conf loaded

--- src/test/suite/config.integration.test.ts
+++ src/test/suite/config.integration.test.ts
@@ -2,9 +2,12 @@ import { Config } from '../../config'
 import * as assert from 'assert'
 
 suite('Config', () => {
-  test('creates config', () => {
+  test('creates config', async () => {
     const c = new Config('flowCommand', 2, 'strict')
 
     assert.strictEqual(c.accounts.length, 0)
+    assert.strictEqual(c.getActiveAccount(), null)
+
+    assert.strictEqual(await c.readLocalConfig(), true)
   })
 })
