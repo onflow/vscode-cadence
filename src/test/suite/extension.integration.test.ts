8c3a36f1ad023d00d04d2c3ea55e0851d684f00d
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2021-08-12 15:54:38 +0200

    linted and added nft

--- src/test/suite/extension.integration.test.ts
+++ src/test/suite/extension.integration.test.ts
@@ -1,11 +1,9 @@
 import * as assert from 'assert'
-import * as fs from 'fs'
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode'
-import * as cmd from '../../commands'
-import {Config} from "../../config";
+import { Config } from '../../config'
 
 suite('Extension Test Suite', () => {
   vscode.window.showInformationMessage('Start all tests.')
@@ -22,11 +20,11 @@ suite('Extension Test Suite', () => {
 
   test('Extension is registered', async () => {
     const ext = vscode.extensions.getExtension('onflow.cadence')
-    await ext!.activate()
+    await ext?.activate()
 
-    assert.strictEqual(ext!.isActive, true)
+    assert.strictEqual(ext?.isActive, true)
 
-    await vscode.commands.executeCommand('cadence.restartServer')
-
+    const r = await vscode.commands.executeCommand('cadence.restartServer')
+    console.log('##', r)
   }).timeout(10000)
 })
