257bb2aa22e287dd397ec5933c6853bdc16f21cf
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2021-08-12 15:38:49 +0200

    merge integration tests and register test

--- src/test/suite/extension.integration.test.ts
+++ src/test/suite/extension.integration.test.ts
@@ -1,16 +1,32 @@
 import * as assert from 'assert'
+import * as fs from 'fs'
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode'
-// import * as myExtension from '../../extension';
+import * as cmd from '../../commands'
+import {Config} from "../../config";
 
 suite('Extension Test Suite', () => {
   vscode.window.showInformationMessage('Start all tests.')
     .then(() => {}, () => {})
 
-  test('Basic test', () => {
-    assert.strictEqual([1, 2, 3].indexOf(5), -1)
-    assert.strictEqual([1, 2, 3].indexOf(0), -1)
+  test('creates config', async () => {
+    const c = new Config('flowCommand', 2, 'strict')
+
+    assert.strictEqual(c.accounts.length, 0)
+    assert.strictEqual(c.getActiveAccount(), null)
+
+    assert.strictEqual(await c.readLocalConfig(), true)
   })
+
+  test('Extension is registered', async () => {
+    const ext = vscode.extensions.getExtension('onflow.cadence')
+    await ext!.activate()
+
+    assert.strictEqual(ext!.isActive, true)
+
+    await vscode.commands.executeCommand('cadence.restartServer')
+
+  }).timeout(10000)
 })
