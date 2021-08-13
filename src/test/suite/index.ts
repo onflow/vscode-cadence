7abf44b78db66d67daa6401e3a1b90fc13b6f62b
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2021-08-13 12:10:04 +0200

    delay helper

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -35,3 +35,9 @@ export async function run (): Promise<void> {
     })
   })
 }
+
+export async function delay (seconds: number): Promise<void> {
+  await new Promise((resolve, reject) => {
+    setTimeout(() => resolve(), seconds * 1000)
+  })
+}
