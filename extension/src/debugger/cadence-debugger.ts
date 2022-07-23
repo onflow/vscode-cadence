6bf8ba1074daa425047233e897a9bfdc70aa3297
Dylan Tinianov <dylantinianov@gmail.com>
2022-07-23 00:05:52 -0400

    Update cadence debugger

--- extension/src/debugger/cadence-debugger.ts
+++ extension/src/debugger/cadence-debugger.ts
@@ -22,19 +22,19 @@ export class CadenceDebugger {
   #registerDebugger (ctx: ExtensionContext): void {
     const debugOutputChannel = window.createOutputChannel('Cadence Debug')
     this.debugAdapters.push(debugOutputChannel) // TODO: can we subscribe there, or do we need to hold onto them?
-    //ctx.subscriptions.push(debugOutputChannel) 
+    // ctx.subscriptions.push(debugOutputChannel)
 
     const provider = new CadenceDebugConfigurationProvider()
     this.debugAdapters.push(debug.registerDebugConfigurationProvider('flow-emulator', provider))
-    //ctx.subscriptions.push(debug.registerDebugConfigurationProvider('flow-emulator', provider))
+    // ctx.subscriptions.push(debug.registerDebugConfigurationProvider('flow-emulator', provider))
 
     const tracker = new CadenceDebugAdapterTrackerFactory(debugOutputChannel)
     this.debugAdapters.push(debug.registerDebugAdapterTrackerFactory('flow-emulator', tracker))
 
-    //ctx.subscriptions.push(debug.registerDebugAdapterTrackerFactory('flow-emulator', tracker))
+    // ctx.subscriptions.push(debug.registerDebugAdapterTrackerFactory('flow-emulator', tracker))
 
     const factory = new CadenceDebugAdapterDescriptorFactory(debugOutputChannel)
     this.debugAdapters.push(debug.registerDebugAdapterDescriptorFactory('flow-emulator', factory))
-    //ctx.subscriptions.push(debug.registerDebugAdapterDescriptorFactory('flow-emulator', factory))
+    // ctx.subscriptions.push(debug.registerDebugAdapterDescriptorFactory('flow-emulator', factory))
   }
 }
