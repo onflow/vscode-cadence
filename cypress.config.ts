4dda61ae0ac741973c9acca0adf8104c1d6555d9
Dylan Tinianov <dylantinianov@gmail.com>
2022-08-04 09:40:09 -0400

    Update Cypress to 10.0.0

---
+++ cypress.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  projectId: "3ei47t",
+  //integrationFolder: "extension/test/e2e/",
+  video: true,
+  screenshotOnRunFailure: true
+})
