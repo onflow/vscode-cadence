709ca2190f7210f7c3f689864213f83cb9fd38ba
Dylan Tinianov <dylantinianov@gmail.com>
2022-08-16 14:25:21 -0400

    Upsate user info

--- extension/src/telemetry/mixpanel-wrapper.ts
+++ extension/src/telemetry/mixpanel-wrapper.ts
@@ -1,6 +1,5 @@
 /* Wrapper functions for Mixpanel analytics */
 import * as mixpanel from 'mixpanel'
-import { countries, zones } from 'moment-timezone/data/meta/latest.json'
 import osName from 'os-name'
 
 // Mixpanel vscode-cadence
@@ -16,8 +15,6 @@ let mixPanel: mixpanel.Mixpanel | undefined
 interface UserInfo {
   vscode_cadence_version: string
   distinct_id: string
-  city: string
-  country: string
   operating_system: string
 }
 let userInfo: UserInfo | undefined
@@ -29,34 +26,10 @@ export async function mixpanelInit (activate: boolean): Promise<void> {
 
 // Set user information including uid, city, country, and operating system
 export function setUserInformation (uid: string, version: string): void {
-  const userOS: string = osName()
-  let userCity: string = ''
-  let userCountry: string = ''
-
-  try {
-    const timeZoneCityToCountry: {[key: string]: string} = {}
-
-    Object.keys(zones).forEach(z => {
-      const cityArr = z.split('/')
-      const city: string = cityArr[cityArr.length - 1]
-      // @ts-expect-error
-      timeZoneCityToCountry[city] = countries[zones[z].countries[0]].name
-    })
-
-    const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone
-    const tzArr = userTimeZone.split('/')
-    userCity = tzArr[tzArr.length - 1]
-    userCountry = timeZoneCityToCountry[userCity]
-  } catch (err) {
-    void err
-  }
-
   userInfo = {
     vscode_cadence_version: version,
     distinct_id: uid,
-    city: userCity,
-    country: userCountry,
-    operating_system: userOS
+    operating_system: osName()
   }
 }
 
@@ -65,8 +38,6 @@ export function captureStatistics (eventName: string, properties: mixpanel.Prope
   if (userInfo !== undefined) {
     properties.vscode_cadence_version = userInfo.vscode_cadence_version
     properties.distinct_id = userInfo.distinct_id
-    properties.$city = userInfo.city
-    properties.mp_country_code = userInfo.country
     properties.$os = userInfo.operating_system
   }
   mixPanel.track(eventName, properties)
