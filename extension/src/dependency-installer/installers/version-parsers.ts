19385b81bf6c9aec1530b93641541611cf9beb95
Dylan Tinianov <dylantinianov@gmail.com>
2022-11-22 11:00:01 -0500

    fix lint

--- extension/src/dependency-installer/installers/version-parsers.ts
+++ extension/src/dependency-installer/installers/version-parsers.ts
@@ -1,5 +1,5 @@
 /* Contains functions for parsing. Should be tested in parser.unit.test.ts */
 
-export function parseFlowCliVersion(buffer: Buffer): string {
+export function parseFlowCliVersion (buffer: Buffer): string {
   return (buffer.toString().split('\n')[0]).split(' ')[1]
 }
--- extension/test/unit/parser.unit.test.ts
+++ extension/test/unit/parser.unit.test.ts
@@ -3,11 +3,11 @@ import { ASSERT_EQUAL } from './test-utils'
 
 describe('Parser function tests', () => {
   it('flow-cli version parsing', () => {
-    let versionTest: Buffer = Buffer.from("Version: v0.1.0\nCommit: 0a1b2c3d")
+    let versionTest: Buffer = Buffer.from('Version: v0.1.0\nCommit: 0a1b2c3d')
     let formatted = parseFlowCliVersion(versionTest)
     ASSERT_EQUAL(formatted, 'v0.1.0')
 
-    versionTest = Buffer.from("Version: v0.1.0")
+    versionTest = Buffer.from('Version: v0.1.0')
     formatted = parseFlowCliVersion(versionTest)
     ASSERT_EQUAL(formatted, 'v0.1.0')
   })
