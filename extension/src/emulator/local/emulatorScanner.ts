ed7aa5a65d3c70735dc553f44a2023b6c2bc58bf
Dylan Tinianov <dylantinianov@gmail.com>
2023-01-31 10:54:39 -0500

    Find emulator runpath

--- extension/src/emulator/local/emulatorScanner.ts
+++ extension/src/emulator/local/emulatorScanner.ts
@@ -4,6 +4,10 @@ import find = require('find-process');
 const { promisify } = require('util');
 const exec = promisify(require('child_process').exec)
 import { window } from 'vscode'
+import * as Config from './config'
+import os = require("os")
+
+let showLocationWarning = true
 
 export async function emulatorExists (): Promise<boolean> {
   const defaultHost = '127.0.0.1'
@@ -15,48 +19,78 @@ export async function emulatorExists (): Promise<boolean> {
   }
 
   if (status !== 'open') {
+    showLocationWarning = true
     return false
   }
 
-  // TODO: Only consider an emulator as valid if it's in the same dir as flow.json
-  // TODO: Otherwise the LS will crash anyways, so we shouldn't connect to it and 
-  // TODO: give a warning instead
+  // Only connect to emulator if running from same dir as flow.json or else LS will crash
   if (!await validEmulatorLocation()) {
-    window.showWarningMessage(`Emulator detected running in a different directory than your flow.json 
-    config. To connect an emulator, please run 'flow emulator' in the same directory as your flow.json`)
+    if (showLocationWarning) {
+      window.showWarningMessage(`Emulator detected running in a different directory than your flow.json 
+      config. To connect an emulator, please run 'flow emulator' in the same directory as your flow.json`)
+      showLocationWarning = false // Avoid spamming the location warning
+    }
+    return false
   }
 
+  showLocationWarning = true
 
-
-  return status === 'open'
+  return true
 }
 
-// Warn user if running emulator in different directory from flow.json
-async function validEmulatorLocation (configPath: string): Promise<boolean> {
+async function validEmulatorLocation (): Promise<boolean> {
+  const configPath = await Config.getConfigPath()
   let flowJsonDir = configPath.substring(0, configPath.lastIndexOf('/'))
-  let emulatorDir = await emulatorRunPath()
+  let emulatorDir: string | undefined = undefined
+
+  switch (os.platform()) {
+    case 'darwin':
+    case 'linux':
+      emulatorDir = await emulatorRunPath()
+    case 'win32':
+      emulatorDir = await emulatorRunPathWindows()
+    default:
+      console.log('Cannot find emulator runpath on ', os.platform())
+  }
+
   return emulatorDir === flowJsonDir
 }
 
-
-// Tries to return the path where flow emulator was run from
 export async function emulatorRunPath (): Promise<string | undefined> {
-  // TODO: Find path on Windows as well!!
-
-
   try {
     let emuProccessInfo = (await find('name', 'flow emulator'))
-
-    // TODO: Check if empty
     if (!emuProccessInfo) {
+      console.log('No running flow emulator process')
       return undefined
     }
 
+    const pid = emuProccessInfo[0].pid
+    const cwdIndex = 8 // Runpath dir index in lsof command
+    let output = await exec(`lsof -p ${pid} | grep cwd`)
+    let emulatorPath: string = output.stdout.trim().split(/\s+/)[cwdIndex]
+
+    return emulatorPath
+  } catch (err) {
+    return undefined
+  }
+}
+
+export async function emulatorRunPathWindows (): Promise<string | undefined> {
+  // TODO: Find path on Windows as well!!
+
+  try {
+    let emuProccessInfo = (await find('name', 'flow emulator'))
+    if (!emuProccessInfo) {
+      console.log('No running flow emulator process')
+      return undefined
+    }
+    
     const pid = emuProccessInfo[0].pid
     const cwdIndex = 8 // Dir index in lsof command
     let output = await exec(`lsof -p ${pid} | grep cwd`)
     let emulatorPath: string = output.stdout.trim().split(/\s+/)[cwdIndex]
 
+    console.log('Flow emulator with pid: ', pid, ' is running from: ', emulatorPath)
     return emulatorPath
   } catch (err) {
     return undefined
