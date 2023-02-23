4e61b8bd77c8801baa3b8f45698a901acce25209
Dylan Tinianov <dylantinianov@gmail.com>
2023-02-23 16:03:26 -0500

    Skip workspace settings

--- extension/test/mock/mockSettings.ts
+++ extension/test/mock/mockSettings.ts
@@ -2,7 +2,7 @@ import { Settings } from '../../src/settings/settings'
 import * as path from 'path'
 
 export function getMockSettings (): Settings {
-  const mockSettings: Settings = new Settings()
+  const mockSettings: Settings = new Settings(true)
   mockSettings.accessCheckMode = 'strict'
   mockSettings.customConfigPath = path.join(__dirname, '../integration/fixtures/workspace/flow.json')
   mockSettings.flowCommand = 'flow'
