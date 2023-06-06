2f9709f83ce7b1159f805e0de06bc03f07f4d4ef
Jordan Ribbink <jribbink@telus.net>
2023-06-05 21:34:24 -0700

    fix windows

--- extension/test/integration/0 - dependencies.test.ts
+++ extension/test/integration/0 - dependencies.test.ts
@@ -1,7 +1,6 @@
 import * as assert from 'assert'
 import * as depInstaller from '../../src/dependency-installer/dependency-installer'
 import { MaxTimeout } from '../globals'
-import { restartVscode } from '../../src/utils/utils'
 import * as os from 'os'
 
 // Note: Dependency installation must run before other integration tests
@@ -11,10 +10,8 @@ suite('Dependency Installer', () => {
       const dependencyManager = new depInstaller.DependencyInstaller()
       await assert.doesNotReject(async () => { await dependencyManager.installMissing() })
 
-      if (os.platform() === 'win32') {
-        // Restart extension and check that all dependencies are still installed
-        await restartVscode()
-      } else {
+      // If not on Windows, check that all dependencies are installed
+      if (os.platform() !== 'win32') {
         // Check that all dependencies are installed
         await dependencyManager.checkDependencies()
         assert.deepStrictEqual(await dependencyManager.missingDependencies.getValue(), [])
