d1fa28aa05430377f6161be303ba3ccb0196b4e2
Jordan Ribbink <jribbink@telus.net>
2023-06-09 08:40:11 -0700

    Change readme

--- extension/src/utils/exec-system.ts
+++ extension/src/utils/exec-system.ts
@@ -1,12 +1,12 @@
 import { exec } from 'child_process'
 import { promisify } from 'util'
 
-// Execute a command in powershell; returns false on error
+// Execute a command in powershell
 export async function execPowerShell (cmd: string): Promise<{ stdout: string, stderr: string }> {
   return await promisify(exec)(cmd, { shell: 'powershell.exe' })
 }
 
-// Execute command in default shell; returns false on error
+// Execute command in default shell
 export async function execDefault (cmd: string): Promise<{ stdout: string, stderr: string }> {
   return await promisify(exec)(cmd)
 }
