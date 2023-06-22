cd29ebfd0c862144f059fd65748a597e4074fdb2
Jordan Ribbink <jribbink@telus.net>
2023-06-21 17:25:52 -0700

    try pressing enter

--- extension/test/integration/0 - dependencies.test.ts
+++ extension/test/integration/0 - dependencies.test.ts
@@ -1,12 +1,25 @@
 import * as assert from 'assert'
 import {DependencyInstaller} from '../../src/dependency-installer/dependency-installer'
 import { MaxTimeout } from '../globals'
+import { window } from 'vscode'
 
 // Note: Dependency installation must run before other integration tests
 suite('Dependency Installer', () => {
   test('Install Missing Dependencies', async () => {
     const dependencyManager = new DependencyInstaller()
+    const timeouts = []
+    timeouts.push(setTimeout(() => window.activeTerminal?.sendText(''), 1000))
+    timeouts.push(setTimeout(() => window.activeTerminal?.sendText(''), 2000))
+    timeouts.push(setTimeout(() => window.activeTerminal?.sendText(''), 3000))
+    timeouts.push(setTimeout(() => window.activeTerminal?.sendText(''), 4000))
+    timeouts.push(setTimeout(() => window.activeTerminal?.sendText(''), 5000))
+    timeouts.push(setTimeout(() => window.activeTerminal?.sendText(''), 6000))
+    timeouts.push(setTimeout(() => window.activeTerminal?.sendText(''), 7000))
+    timeouts.push(setTimeout(() => window.activeTerminal?.sendText(''), 8000))
+    timeouts.push(setTimeout(() => window.activeTerminal?.sendText(''), 9000))
     await assert.doesNotReject(async () => { await dependencyManager.installMissing() })
+
+    timeouts.forEach(timeout => clearTimeout(timeout))
 
     // Check that all dependencies are installed
     await dependencyManager.checkDependencies()
