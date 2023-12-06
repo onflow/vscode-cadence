e26ac0626c2cda9c4f9f3ed67589ba92c43e9113
Jordan Ribbink <jribbink@telus.net>
2023-12-05 22:20:37 -0800

    remove useless var

--- extension/src/test-provider/constants.ts
+++ extension/src/test-provider/constants.ts
@@ -1,5 +1,4 @@
 import * as vscode from 'vscode'
 
 export const CADENCE_TEST_TAG = new vscode.TestTag('cadence')
-export const CADENCE_TEST_COMMAND = 'flow test'
 export const TEST_FUNCTION_PREFIX = ':'
