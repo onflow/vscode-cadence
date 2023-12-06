f148f2e8d578a95e92e9f2b020f9fa61a6fe6e3f
Jordan Ribbink <jribbink@telus.net>
2023-12-05 22:21:46 -0800

    format

--- extension/src/crypto-polyfill.ts
+++ extension/src/crypto-polyfill.ts
@@ -10,7 +10,7 @@ globalThis.crypto = {
       const e = new Error()
       // @ts-expect-error
       e.code = 22
-      e.message = `Failed to execute \'getRandomValues\' on \'Crypto\': The 'ArrayBufferView\'s byte length (${buf.length}) exceeds the number of bytes of entropy available via this API (65536).`
+      e.message = `Failed to execute 'getRandomValues' on 'Crypto': The 'ArrayBufferView's byte length (${buf.length}) exceeds the number of bytes of entropy available via this API (65536).`
       e.name = 'QuotaExceededError'
       throw e
     }
