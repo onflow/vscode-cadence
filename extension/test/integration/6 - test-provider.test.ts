86cf0adee271aece490e02f816216a836a6e129c
Jordan Ribbink <jribbink@telus.net>
2023-12-07 23:48:47 -0800

    Add tests for adding/removing files

--- extension/test/integration/6 - test-provider.test.ts
+++ extension/test/integration/6 - test-provider.test.ts
@@ -14,6 +14,7 @@ suite('test provider tests', () => {
   let mockSettings: Settings
   let mockConfig: FlowConfig
   let testProvider: TestProvider
+  let cleanupFunctions: (() => void | Promise<void>)[] = []
 
   beforeEach(async function () {
     this.timeout(5000)
@@ -42,6 +43,10 @@ suite('test provider tests', () => {
     this.timeout(5000)
 
     testProvider.dispose()
+    for (const cleanupFunction of cleanupFunctions) {
+      await cleanupFunction()
+    }
+    cleanupFunctions = []
   })
 
   test('runs all tests in workspace and reports results', async function () {
@@ -109,5 +114,101 @@ suite('test provider tests', () => {
       { filepath: path.join(workspacePath, 'test/test1.cdc'), id: ':testPassing' }
     ])
     assert.deepStrictEqual(failedTests, [])
+  }).timeout(20000)
+
+  test('runs tests including newly created file', async function () {
+    // Create new file
+    const testFilePath = path.join(workspacePath, 'test/bar/test4.cdc')
+    const testFileContents = `
+      import Test
+      pub fun testPassing() {
+        Test.assert(true)
+      }
+    `
+    await vscode.workspace.fs.writeFile(vscode.Uri.file(testFilePath), Buffer.from(testFileContents))
+    cleanupFunctions.push(async () => {
+      await vscode.workspace.fs.delete(vscode.Uri.file(testFilePath))
+    })
+    await new Promise<void>(resolve => setTimeout(resolve, 1000))
+
+    // Run tests
+    let runSpy: sinon.SinonSpiedInstance<vscode.TestRun> | undefined
+    await new Promise<void>(resolve => {
+      void testProvider.runAllTests(undefined, (testRun) => {
+        const originalEnd = testRun.end
+        testRun.end = () => {
+          originalEnd.call(testRun)
+          resolve()
+        }
+
+        runSpy = sinon.spy(testRun)
+        return runSpy
+      }) 
+    })
+    if (runSpy == null) throw new Error('runSpy is null')
+
+    const passedTests = runSpy.passed.getCalls().map(call => ({ filepath: (call.args[0].uri as vscode.Uri).fsPath, id: call.args[0].id }))
+    const failedTests = runSpy.failed.getCalls().map(call => ({ filepath: (call.args[0].uri as vscode.Uri).fsPath, id: call.args[0].id, message: (call.args[1] as any).message }))
+
+    passedTests.sort((a, b) => a.filepath.localeCompare(b.filepath))
+    failedTests.sort((a, b) => a.filepath.localeCompare(b.filepath))
+
+    assert.strictEqual(passedTests.length + failedTests.length, 6)
+    assert.deepStrictEqual(passedTests, [
+      { filepath: path.join(workspacePath, 'test/bar/test2.cdc'), id: ':testPassing' },
+      { filepath: path.join(workspacePath, 'test/bar/test3.cdc'), id: ':testPassing' },
+      { filepath: path.join(workspacePath, 'test/bar/test4.cdc'), id: ':testPassing' },
+      { filepath: path.join(workspacePath, 'test/test1.cdc'), id: ':testPassing' }
+    ])
+    assert.deepStrictEqual(failedTests, [
+      { filepath: path.join(workspacePath, 'test/bar/test2.cdc'), id: ':testFailing', message: 'FAIL: Execution failed:\nerror: assertion failed\n --> 7465737400000000000000000000000000000000000000000000000000000000:8:2\n' },
+      { filepath: path.join(workspacePath, 'test/bar/test3.cdc'), id: ':testFailing', message: 'FAIL: Execution failed:\nerror: assertion failed\n --> 7465737400000000000000000000000000000000000000000000000000000000:4:2\n' }
+    ])
+
+    vscode.workspace.updateWorkspaceFolders(0, 1)
+  }).timeout(20000)
+
+  test('runs tests including newly created file', async function () {
+    // Delete test file
+    const testFilePath = path.join(workspacePath, 'test/bar/test3.cdc')
+    const originalContents = await vscode.workspace.fs.readFile(vscode.Uri.file(testFilePath))
+    cleanupFunctions.push(async () => {
+      await vscode.workspace.fs.writeFile(vscode.Uri.file(testFilePath), originalContents)
+    })
+    await new Promise<void>(resolve => setTimeout(resolve, 1000))
+
+    // Run tests
+    let runSpy: sinon.SinonSpiedInstance<vscode.TestRun> | undefined
+    await new Promise<void>(resolve => {
+      void testProvider.runAllTests(undefined, (testRun) => {
+        const originalEnd = testRun.end
+        testRun.end = () => {
+          originalEnd.call(testRun)
+          resolve()
+        }
+
+        runSpy = sinon.spy(testRun)
+        return runSpy
+      }) 
+    })
+    if (runSpy == null) throw new Error('runSpy is null')
+
+    const passedTests = runSpy.passed.getCalls().map(call => ({ filepath: (call.args[0].uri as vscode.Uri).fsPath, id: call.args[0].id }))
+    const failedTests = runSpy.failed.getCalls().map(call => ({ filepath: (call.args[0].uri as vscode.Uri).fsPath, id: call.args[0].id, message: (call.args[1] as any).message }))
+
+    passedTests.sort((a, b) => a.filepath.localeCompare(b.filepath))
+    failedTests.sort((a, b) => a.filepath.localeCompare(b.filepath))
+
+    assert.strictEqual(passedTests.length + failedTests.length, 4)
+    assert.deepStrictEqual(passedTests, [
+      { filepath: path.join(workspacePath, 'test/bar/test2.cdc'), id: ':testPassing' },
+      { filepath: path.join(workspacePath, 'test/test1.cdc'), id: ':testPassing' }
+    ])
+    assert.deepStrictEqual(failedTests, [
+      { filepath: path.join(workspacePath, 'test/bar/test2.cdc'), id: ':testFailing', message: 'FAIL: Execution failed:\nerror: assertion failed\n --> 7465737400000000000000000000000000000000000000000000000000000000:8:2\n' },
+      { filepath: path.join(workspacePath, 'test/bar/test3.cdc'), id: ':testFailing', message: 'FAIL: Execution failed:\nerror: assertion failed\n --> 7465737400000000000000000000000000000000000000000000000000000000:4:2\n' }
+    ])
+
+    vscode.workspace.updateWorkspaceFolders(0, 1)
   }).timeout(20000)
 })
