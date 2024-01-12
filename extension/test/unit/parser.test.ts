cd66c97a5cc59a7914a5daea1266b5cf3da10ebb
Jordan Ribbink <jribbink@telus.net>
2024-01-12 12:31:00 -0800

    fix tests

--- extension/test/unit/parser.test.ts
+++ extension/test/unit/parser.test.ts
@@ -1,14 +1,24 @@
-import { parseFlowCliVersion } from '../../src/utils/flow-version'
+import { extractFlowCLIVersion } from '../../src/utils/flow-version'
 import { ASSERT_EQUAL } from '../globals'
 
 suite('Parsing Unit Tests', () => {
-  test('Flow CLI Version Parsing', async () => {
+  test('Flow CLI Version Extraction', async () => {
     let versionTest: Buffer = Buffer.from('Version: v0.1.0\nCommit: 0a1b2c3d')
-    let formatted = parseFlowCliVersion(versionTest)
+    let formatted = extractFlowCLIVersion(versionTest)
     ASSERT_EQUAL(formatted, 'v0.1.0')
 
     versionTest = Buffer.from('Version: v0.1.0')
-    formatted = parseFlowCliVersion(versionTest)
+    formatted = extractFlowCLIVersion(versionTest)
     ASSERT_EQUAL(formatted, 'v0.1.0')
+  })
+
+  test('Flow CLI Version Extraction (Fallback)', async () => {
+    let versionTest: Buffer = Buffer.from('somethingtobreak the code\nv0.1.0\nCommit: 0a1b2c3d')
+    let formatted = extractFlowCLIVersion(versionTest)
+    ASSERT_EQUAL(formatted, '0.1.0')
+
+    versionTest = Buffer.from('v0.1.0')
+    formatted = extractFlowCLIVersion(versionTest)
+    ASSERT_EQUAL(formatted, '0.1.0')
   })
 })
