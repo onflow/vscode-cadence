7d66ec0a8802055ee1a357b1d5d5a66783f5a1c9
Jordan Ribbink <jribbink@telus.net>
2024-01-31 14:45:40 -0800

    rename tests

--- extension/test/unit/parser.test.ts
+++ extension/test/unit/parser.test.ts
@@ -5,7 +5,7 @@ import { ASSERT_EQUAL } from '../globals'
 import * as semver from 'semver'
 
 suite('Parsing Unit Tests', () => {
-  test('Flow CLI Version Parsing Buffer', async () => {
+  test('Flow CLI Version Parsing (buffer input)', async () => {
     let versionTest: Buffer = Buffer.from('Version: v0.1.0\nCommit: 0a1b2c3d')
     let formatted = parseFlowCliVersion(versionTest)
     ASSERT_EQUAL(formatted, 'v0.1.0')
@@ -15,13 +15,13 @@ suite('Parsing Unit Tests', () => {
     ASSERT_EQUAL(formatted, 'v0.1.0')
   })
 
-  test('FLow CLI Version Parsing string', async () => {
+  test('Flow CLI Version Parsing (string input)', async () => {
     const versionTest: string = 'Version: v0.1.0\nCommit: 0a1b2c3d'
     const formatted = parseFlowCliVersion(versionTest)
     ASSERT_EQUAL(formatted, 'v0.1.0')
   })
 
-  test('Flow CLI Version Parsing Integration', async () => {
+  test('Flow CLI Version Parsing produces valid semver from Flow CLI output', async () => {
     // Check that version is parsed from currently installed flow-cli
     const { stdout } = await execDefault('flow version')
     const formatted = parseFlowCliVersion(stdout)
