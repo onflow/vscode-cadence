59e57a6bcd1762102c4c84d4e75935aec1a90ac0
Jordan Ribbink <jribbink@telus.net>
2024-04-15 16:37:06 -0700

    format

--- extension/src/dependency-installer/installers/flow-cli-installer.ts
+++ extension/src/dependency-installer/installers/flow-cli-installer.ts
@@ -129,7 +129,7 @@ export class InstallFlowCLI extends Installer {
     // Check if flow version is valid to verify install
     this.#context.cliProvider.refresh()
     const installedVersions = await this.#context.cliProvider.getBinaryVersions().catch((e) => {
-      window.showErrorMessage('Failed to check CLI version: ' + e.message)
+      void window.showErrorMessage(`Failed to check CLI version: ${String(e.message)}`)
       return []
     })
     const version = installedVersions.find(y => y.command === KNOWN_FLOW_COMMANDS.DEFAULT)?.version
